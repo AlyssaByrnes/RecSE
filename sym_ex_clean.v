10114ebac63f96512d7af2cc1a40794d8a760a8a
abyrnes1 <abyrnes1@alyssas-laptop.attlocal.net>
2018-08-09 23:05:59 -0400

    Added cleaner, more abstract version

---
+++ sym_ex_clean.v
@@ -0,0 +1,236 @@
+Module ConcState.
+
+Inductive conc_state : Type :=
+|concstate.
+
+
+Inductive conc_state_set : Type :=
+|Empty 
+|Cons (x : conc_state) (s: conc_state_set).
+
+(* conc_ex(A) returns ConcState that results from 
+the concrete execution of ConcState A  *)
+Definition concrete_execution := conc_state_set -> conc_state_set.
+
+Axiom conc_ex : concrete_execution.
+
+
+Fixpoint In (A:conc_state_set) (x:conc_state) : Prop := 
+match A with 
+|Empty => False
+|Cons y s => (y=x) \/ In s x
+end.
+
+
+End ConcState.
+
+Import ConcState.
+
+
+Module SymState.
+(* Symbolic state contains abstract state 
+and path constraint. *)
+
+
+Variable Phi PC : Type.
+
+Inductive sym_state: Type :=
+|ConstructState (a : Phi) (p : PC)
+|SymEx (x : sym_state).
+
+
+Definition up_pc := PC -> PC.
+Axiom update_pc : up_pc.
+
+Definition up_phi := Phi -> Phi.
+Axiom update_phi: up_phi.
+
+(* get_phi returns abstract state *)
+Definition get_sym_state  :=  sym_state -> Phi.
+Axiom get_phi : get_sym_state.
+
+(* get_pc returns the path constraint *)
+Definition get_path_constraint := sym_state -> PC.
+Axiom get_pc : get_path_constraint.
+
+Definition sym_execution := sym_state -> sym_state.
+Axiom sym_ex : sym_execution.
+
+Fixpoint sym_ex_n (x:sym_state) (n:nat) : sym_state :=
+match n with
+|0 => x
+|S n' => sym_ex (sym_ex_n x (n'))
+end.
+
+
+Definition uni := sym_state -> ConcState.conc_state_set.
+Axiom unif : uni.
+
+
+End SymState.
+
+Import SymState. 
+
+
+
+
+
+Module System.
+(* System initializes with a defined set of
+ initial configuration states, InitStates *)
+
+Definition conc_ex (A: ConcState.conc_state_set) : ConcState.conc_state_set := 
+conc_ex A.
+
+Fixpoint conc_ex_n (x: ConcState.conc_state) (n:nat) : ConcState.conc_state_set :=
+match n with
+|0 => Cons x Empty
+|S n' => conc_ex (conc_ex_n x (n'))
+end.
+
+End System.
+
+Import System. 
+
+Module SETree.
+(* is_leaf(T, n) returns true if
+ n is a leaf in tree T. *)
+(* is_root(T, n) returns true if
+ n is a root in tree T. *)
+(* get_root(T) returns the root of tree T. *)
+(*Modified version of FSet RBT https://github.com/coq-contribs/fsets/blob/master/FSetRBT.v *)
+Definition state := SymState.sym_state.
+
+Inductive SE_tree : Type :=
+| leaf: SE_tree
+| sym_node: SE_tree -> state -> SE_tree -> SE_tree
+| intermediate_node : SE_tree -> state ->SE_tree.
+
+Fixpoint max (m n : nat) : nat :=
+match m, n with
+|0, _ => n
+|S m', 0 => m
+|S m', S n' => S (max m' n')
+end.
+
+Fixpoint tree_height (t : SE_tree) : nat :=
+match t with
+|leaf => O
+|sym_node l n r  => S (max (tree_height l) (tree_height r))
+|intermediate_node child n => S (tree_height child)
+end.
+
+Inductive SE_tree_list : Type :=
+|nil: SE_tree_list
+|cons: SE_tree -> SE_tree_list -> SE_tree_list.
+
+Notation "x :: l" := (cons x l) (at level 60, right associativity).
+Notation "[ ]" := nil.
+Notation "[ x ; .. ; y ]" := (cons x .. (cons y nil) ..).
+
+Fixpoint length (l:SE_tree_list) : nat := 
+  match l with
+  | nil => O
+  | h :: t => S (length t)
+  end.
+
+
+Fixpoint in_tree_list  (tlist : SE_tree_list) (x : SETree.SE_tree) : Prop :=
+match tlist with 
+|nil => False
+|h :: t => (x = h) \/ (in_tree_list t x)
+end.
+
+
+
+
+Definition is_leaf (T: SE_tree) (n : SymState.sym_state) : Prop := True.
+
+
+
+(* SE Properties Go Here *)
+Axiom lem_1 : forall (conc1 conc2 : ConcState.conc_state)
+ (sym1: SymState.sym_state),
+(conc_ex (Cons conc1 Empty) = (Cons conc2 Empty)) /\ (In (unif sym1) conc1)  ->
+exists sym2, 
+(In (unif sym2) conc2) /\ sym_ex sym1 = sym2. 
+
+Axiom lem_1_n : forall (conc1 conc2 : ConcState.conc_state)
+ (sym1: SymState.sym_state) (n : nat),
+(conc_ex_n conc1 n = (Cons conc2 Empty)) /\ (In (unif sym1) conc1)  ->
+exists sym2, 
+(In (unif sym2) conc2) /\ sym_ex_n sym1 n = sym2.
+
+Axiom lem_2 : forall (conc2 : ConcState.conc_state)
+ (sym1 sym2: SymState.sym_state),
+(sym_ex sym1 = sym2) /\ (In (unif sym2) conc2)  ->
+exists conc1, 
+(In (unif sym1) conc1) /\ 
+((conc_ex (Cons conc1 Empty)) = (Cons conc2 Empty)).
+
+Axiom lem_2_n : forall (conc2 : ConcState.conc_state)
+ (sym1 sym2: SymState.sym_state) (n:nat),
+(sym_ex_n sym1 n = sym2) /\ (In (unif sym2) conc2)  ->
+exists conc1, 
+(In (unif sym1) conc1) /\ 
+(conc_ex_n conc1 n = (Cons conc2 Empty)).
+
+
+
+
+
+
+
+
+End SETree.
+
+
+Import SETree. 
+
+
+Module SETreeList. 
+
+
+
+End SETreeList.
+
+Import SETreeList.
+
+Module SERecurs.
+
+
+Definition circle_op_1 (sym : SymState.sym_state) : ConcState.conc_state_set :=
+unif sym.
+
+Definition circle_op_2 (sym : SymState.sym_state) : ConcState.conc_state_set :=
+unif (sym_ex sym).
+
+Fixpoint set_in_set (set1 set2 : ConcState.conc_state_set) : Prop :=
+match set1 with 
+|Empty => (set2 = Empty) \/ False
+|Cons y s => (set1 = set2) \/ (set_in_set s set2)
+end.
+
+Variable init_conc_states: ConcState.conc_state_set.
+
+(* 3 properties and sufficiency go here *)
+Variable ErrorStates: ConcState.conc_state_set.
+
+Variable tree_list : SETree.SE_tree_list.
+(* NEED TREE LIST GENERATION *)
+
+
+Definition is_connected (tlist : SETree.SE_tree_list) : Prop := True.
+
+
+Axiom properties : forall (e : SETree.SE_tree), 
+in_tree_list tree_list e -> 
+exists n,
+(SETree.is_leaf e n)
+/\(set_in_set init_conc_states  (circle_op_1 n))
+/\ (set_in_set  ErrorStates (circle_op_2 n))
+/\ (is_connected tree_list). 
+
+End SERecurs.
+
+
